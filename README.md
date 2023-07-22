8ce52c980313af05c848d985bcfee9e8cb83b2c3
Starcea / 스타샤 <stardev.uwu@gmail.com>
2023-07-22 23:46:22 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# discord.js + command.ts + Jejudo
+> **Warning** Template may be outdated
+> 
+> Check out [cts-template repo](https://github.com/star0202/cts-template) for the latest template
+
+
+```sh
+corepack enable
+pnpm i
+```
+```sh
+pnpm dev
+```
+```sh
+pnpm build
+
+pnpm start
+# or
+pm2 start
+```
---
+++ src/structures/Client.ts
@@ -0,0 +1,71 @@
+import { config } from '../config'
+import { VERSION } from '../constants'
+import { CommandClient } from '@pikokr/command.ts'
+import { green } from 'chalk'
+import { ActivityType, Client } from 'discord.js'
+import { short } from 'git-rev-sync'
+import { Jejudo } from 'jejudo'
+import { join } from 'path'
+import { Logger } from 'tslog'
+
+export default class CustomClient extends CommandClient {
+  private jejudo: Jejudo | null = null
+
+  constructor(logger: Logger<unknown>) {
+    super(
+      new Client({
+        intents: ['GuildMessages', 'Guilds', 'MessageContent'],
+      }),
+      logger
+    )
+
+    this.discord.once('ready', (client) => this.onReady(client))
+
+    this.discord.on('debug', (msg) => {
+      this.logger.debug(msg)
+    })
+  }
+
+  async setup() {
+    await this.enableApplicationCommandsExtension({ guilds: config.guilds })
+
+    await this.registry.loadAllModulesInDirectory(
+      join(__dirname, '..', 'modules')
+    )
+  }
+
+  async onReady(client: Client<true>) {
+    this.jejudo = new Jejudo(client, {
+      isOwner: (user) => this.owners.has(user.id),
+      prefix: `<@${client.user.id}>`,
+      noPermission: (i) => i.reply('Permission denied'),
+    })
+
+    client.on('messageCreate', (msg) => this.jejudo?.handleMessage(msg))
+
+    client.on('interactionCreate', (i) => {
+      this.jejudo?.handleInteraction(i)
+    })
+
+    client.user.setPresence({
+      activities: [
+        {
+          name: `${VERSION} (${short()})`,
+          type: ActivityType.Playing,
+        },
+      ],
+    })
+
+    this.logger.info(`Logged in as: ${green(client.user.tag)}`)
+
+    await this.fetchOwners()
+  }
+
+  async start() {
+    await this.setup()
+
+    await this.discord.login(config.token)
+
+    await this.getApplicationCommandsExtension()?.sync()
+  }
+}
