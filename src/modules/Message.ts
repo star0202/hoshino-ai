dc65e6afa4da5d73281b32feb4b763656495a369
Starcea / 스타샤 <stardev.uwu@gmail.com>
2023-07-23 13:02:16 +0900

    feat: ㄴㅇㄴㅇ

--- src/modules/Message.ts
+++ src/modules/Message.ts
@@ -1,28 +1,38 @@
 import { Extension, listener } from '@pikokr/command.ts'
 import type { Message } from 'discord.js'
 
+const replyWithSticker = async (msg: Message, name: string) => {
+  if (!msg.guild) return
+
+  const sticker = (await msg.guild.stickers.fetch()).find(
+    (s) => s.name === name
+  )
+
+  if (!sticker) return
+
+  await msg.reply({
+    stickers: [sticker],
+    allowedMentions: {
+      repliedUser: false,
+    },
+  })
+}
+
 class MessageExt extends Extension {
   @listener({ event: 'messageCreate' })
   async onMessage(msg: Message) {
-    if (msg.author.bot || !msg.guild) return
+    if (msg.author.bot) return
 
     switch (msg.content) {
-      case 'ㄴㄹㄹㅋㄹㄹ': {
-        const sticker = (await msg.guild.stickers.fetch()).find(
-          (s) => s.name === 'ㄴㄹㄹㅋㄹㄹ'
-        )
-
-        if (!sticker) return
-
-        await msg.reply({
-          stickers: [sticker],
-          allowedMentions: {
-            repliedUser: false,
-          },
-        })
+      case 'ㄴㄹㄹㅋㄹㄹ':
+        await replyWithSticker(msg, 'ㄴㄹㄹㅋㄹㄹ')
 
         break
-      }
+
+      case 'ㄴㅇㄴㅇ':
+        await replyWithSticker(msg, 'ㄴㅇㄴㅇ')
+
+        break
     }
   }
 }
